827774bfea4daee483743e4a3de2feaebf346d55
Suresh <suresh23534@gmail.com>
2018-03-01 10:53:16 -0800

    Responsive_Web_Page
    A Responsive web page which can be compatable in all devices like Desktop, Tablet and mobile.
    This project uses HTML5, CSS/CSS3, JavaScript, JQuery, Bootstrap, AngularJS etc.,

---
+++ Technovert_JS.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+          $(".navbar-fixed-top").css("background-color", "#313c49"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+          $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+        }
+      });
+    });
